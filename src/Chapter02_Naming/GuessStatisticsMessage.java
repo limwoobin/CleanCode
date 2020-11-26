fba4c7629372cf0643473ae6641895793a146494
limwoobin <drogba02@naver.com>
2020-11-26 10:39:16 +0900

    Clean Code
    - 의미있는 맥락을 추가하라 (명명규칙)

---
+++ src/Chapter02_Naming/GuessStatisticsMessage.java
@@ -0,0 +1,31 @@
+package Chapter02_Naming;
+
+public class GuessStatisticsMessage {
+    // 맥락이 불분명한 변수
+    private void printGuessStatistics(char candidate , int count) {
+        String number;
+        String verb;
+        String pluralModifier;
+
+        if (count == 0) {
+            number = "no";
+            verb = "are";
+            pluralModifier = "s";
+        } else if (count == 1) {
+            number = "1";
+            verb = "is";
+            pluralModifier = "";
+        } else {
+            number = Integer.toString(count);
+            verb = "are";
+            pluralModifier = "s";
+        }
+
+        String guessMessage = String.format(
+                "There % %s %s%s" , verb , number , candidate , pluralModifier
+        );
+
+        System.out.println(guessMessage);
+    }
+}
+
