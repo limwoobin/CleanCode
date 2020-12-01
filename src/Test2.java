2977820594ce8c2d6bbb82eea067d720a55c7fbb
limwoobin <drogba02@naver.com>
2020-12-01 14:03:58 +0900

    Clean Code
    - Socket 예제

--- src/Test2.java
+++ src/Test2.java
@@ -1,30 +1,9 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test2 {
     public static void main(String[] args) {
-
-        for (int i=0; i<10; i++) {
-            long current = System.nanoTime();
-            String s = solution("1234" , "5678");
-            System.out.println(s);
-            System.out.println(System.nanoTime() - current);
-        }
-    }
-
-    // 35900
-    public static String solution(String a, String b) {
-        int value1 = Integer.parseInt(a);
-        int value2 = Integer.parseInt(b);
-
-        value1 = value1 + value2;
-        return Integer.toString(value1);
-    }
-
-    // 28900
-    public static String solution2(String a, String b) {
-        return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
-    }
-
-    // 49100
-    public static String solution3(String a, String b) {
-        return String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));
+        List<String> list = new ArrayList<>();
     }
 }
